3511ea728b17505780186268ef0d931742db428f
Francis Tseng <f+accounts@frnsys.com>
2017-01-15 17:26:30 -0500

    swapped in new frontend (not connected to backend yet)

---
+++ frontend/app/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {Campaigns, Campaign, NewCampaign} from './Campaign';
+import {NewUpdate, NewAction} from './Conversation';
+import {Router, Route, Link, IndexRoute, browserHistory} from 'react-router';
+
+// TODO nav?
+const Container = (props) => <div>
+  {props.children}
+</div>;
+
+const NotFound = () => <h1>404.. This page is not found!</h1>;
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={browserHistory}>
+        <Route path='/' component={Container}>
+          <IndexRoute component={Campaigns} />
+          <Route path='new' component={NewCampaign} />
+          <Route path=':id' component={Campaign} />
+          <Route path=':id/action/new' component={NewAction} />
+          <Route path=':id/update/new' component={NewUpdate} />
+        </Route>
+        <Route path='*' component={NotFound} />
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './main',
+  output: {
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        query: {
+          presets: ['es2015', 'stage-2', 'react']
+        }
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      },
+      {
+        test: /\.json$/,
+        loaders: ['json']
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/style.css')
+  ],
+  resolve: {
+    extensions: ['', '.js', '.sass'],
+    modulesDirectories: ['node_modules']
+  },
+  devServer: {
+    historyApiFallback: true
+  }
+};
