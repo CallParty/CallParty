6010e6cd1cb989b5ba40b615426b50d9cec4cb71
Francis Tseng <f+accounts@frnsys.com>
2017-01-16 16:54:53 -0500

    added simple notification system

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 import {Campaigns, Campaign, NewCampaign} from './Campaign';
 import {NewUpdate, NewAction} from './Conversation';
 import {Router, Route, Link, IndexRoute, browserHistory} from 'react-router';
+import NotificationSystem from 'react-notification-system';
 
-// TODO nav?
 const Container = (props) => {
   return <div>
+    <header className="main-header">
+      <Link to="/">CallParty</Link>
+    </header>
     {props.children}
   </div>;
 };
@@ -13,18 +16,39 @@ const Container = (props) => {
 const NotFound = () => <h1>404.. This page is not found!</h1>;
 
 class App extends Component {
+  static childContextTypes = {
+    notify: React.PropTypes.func,
+  }
+
+  getChildContext() {
+    return {
+      notify: this.notify.bind(this),
+    }
+  }
+
+  notify(notification) {
+    this.notifications.addNotification(notification);
+  }
+
+  componentDidMount() {
+    this.notifications = this.refs.notifications;
+  }
+
   render() {
     return (
-      <Router history={browserHistory}>
-        <Route path='/' component={Container}>
-          <IndexRoute component={Campaigns} />
-          <Route path='new' component={NewCampaign} />
-          <Route path=':id' component={Campaign} />
-          <Route path=':id/action/new' component={NewAction} />
-          <Route path=':id/update/new' component={NewUpdate} />
-        </Route>
-        <Route path='*' component={NotFound} />
-      </Router>
+      <main>
+        <Router history={browserHistory}>
+          <Route path='/' component={Container}>
+            <IndexRoute component={Campaigns} />
+            <Route path='new' component={NewCampaign} />
+            <Route path=':id' component={Campaign} />
+            <Route path=':id/action/new' component={NewAction} />
+            <Route path=':id/update/new' component={NewUpdate} />
+          </Route>
+          <Route path='*' component={NotFound} />
+        </Router>
+        <NotificationSystem ref="notifications" style={false} />
+      </main>
     )
   }
 }
