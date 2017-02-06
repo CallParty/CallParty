b4b892801569c5be82410b3ebd6535e177e5df40
Oren Shoham <oren.avraham.shoham@gmail.com>
2017-02-06 16:58:34 -0600

    Add Committee, Subcommittee, and RepresentativeCommittee models

--- backend/app/models/representative.js
+++ backend/app/models/representative.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema
 
-var representativesSchema = new Schema({
+var representativeSchema = new Schema({
   id: String,
   full_name: String,
   first_name: String,
@@ -18,12 +18,37 @@ var representativesSchema = new Schema({
   contact_form: String,
   state_rank: String,
   district: String,
-  bioguide: String,
+  bioguide: { type: String, unique: true },
   govtrack: String,
   wikipedia: String,
   wikidata: String,
   image_url: String
+}, {
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true }
 })
 
-var Reps = mongoose.model('Reps', representativesSchema)
+representativeSchema.virtual('representativeCommittees', {
+  ref: 'RepresentativeCommittee',
+  localField: '_id',
+  foreignField: 'representative'
+})
+
+representativeSchema.virtual('representativeSubcommittees', {
+  ref: 'RepresentativeSubcommittee',
+  localField: '_id',
+  foreignField: 'representative'
+})
+
+representativeSchema.virtual('committees').get(function() {
+  if (!this.representativeCommittees) { return null }
+  return this.representativeCommittees.map(rc => rc.committee)
+})
+
+representativeSchema.virtual('subcommittees').get(function() {
+  if (!this.representativeSubcommittees) { return null }
+  return this.representativeSubcommittees.map(rs => rs.subcommittee)
+})
+
+var Reps = mongoose.model('Reps', representativeSchema)
 module.exports = Reps
