1c193fa997c14933641c26b279369899c18e0534
Oren Shoham <oren.avraham.shoham@gmail.com>
2017-02-07 11:15:22 -0500

    Add instance method to campaignAction model that queries for matching representatives

--- backend/app/models/campaignAction.js
+++ backend/app/models/campaignAction.js
@@ -8,8 +8,8 @@ const campaignActionSchema = new Schema({
   cta: String,
   active: Boolean,
   type: String,
-  memberTypes: Array,
-  parties: Array,
+  memberTypes: [{ type: String, enum: ['rep', 'sen'] }],
+  parties: [{ type: String, enum: ['Democrat', 'Republican', 'Independent'] }],
   committees: Array,
   createdAt: { type: Date, default: () => moment.utc().toDate() },
   campaign: { type: Schema.Types.ObjectId, ref: 'Campaign' }
@@ -29,5 +29,29 @@ campaignActionSchema.virtual('campaignUpdates', {
   localField: '_id',
   foreignField: 'campaignAction'
 })
+
+campaignActionSchema.methods.getRepresentatives = function() {
+  return this.model('Reps')
+    .aggregate()
+    .lookup({ from: 'representativecommittees', localField: '_id', foreignField: 'representative', as: 'representativeCommittees' })
+    .unwind({ path: '$representativeCommittees', preserveNullAndEmptyArrays: true })
+    .lookup({ from: 'committees', localField: 'representativeCommittees.committee', foreignField: '_id', as: 'committees' })
+    .unwind({ path: '$committees', preserveNullAndEmptyArrays: true })
+    .group({
+      _id: '$_id',
+      legislator_type: { $first: '$legislator_type' },
+      party: { $first: '$party' },
+      state: { $first: '$state' },
+      district: { $first: '$district' },
+      official_full: { $first: '$official_full' },
+      committees: { $push: '$committees' }
+    })
+    .match({
+      legislator_type: { $in: this.memberTypes },
+      party: { $in: this.parties },
+      'committees._id': { $in: this.committees }
+    })
+    .exec()
+}
 
 module.exports = mongoose.model('CampaignAction', campaignActionSchema)
