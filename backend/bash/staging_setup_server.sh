f72aeccd4731e31649ac85fc704f3ba1c9da8232
Max Fowler <maxhfowler@gmail.com>
2017-02-06 23:46:12 -0500

    Response to CR

--- backend/bash/staging_setup_server.sh
+++ backend/bash/staging_setup_server.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 BASEDIR=$( cd $(dirname $(dirname $0)) ; pwd -P )
 cd $BASEDIR/devops
-export GCE_INI_PATH=$BASEDIR/devops/gce.ini
-ansible-playbook -i inventory staging_setup_server.yml
+GCE_INI_PATH=$BASEDIR/devops/gce.ini ansible-playbook -i inventory staging_setup_server.yml
