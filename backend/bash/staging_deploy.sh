e962b0670000ef69e8b6f946ef225c94c6c9074a
Max Fowler <maxhfowler@gmail.com>
2017-02-13 01:39:36 -0500

    Change ansible deploy to copy over frontend assets instead of build them on server

--- backend/bash/staging_deploy.sh
+++ backend/bash/staging_deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 BASEDIR=$( cd $(dirname $(dirname $0)) ; pwd -P )
-cd $BASEDIR/devops
 $BASEDIR/bash/test.sh
+cd $BASEDIR/../frontend; npm run build
+cd $BASEDIR/devops
 GCE_INI_PATH=$BASEDIR/devops/gce.ini ansible-playbook -i inventory staging_deploy.yml
 
