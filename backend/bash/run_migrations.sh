4ff3b5653de1415f54d8bb0b000bee5e314e83db
Oren Shoham <oren.avraham.shoham@gmail.com>
2017-03-12 16:15:51 -0400

    Add scripts for creating, listing, running, and rolling back migrations

---
+++ backend/bash/run_migrations.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+BASEDIR=$( cd $(dirname $(dirname $0)) ; pwd -P )
+cd $BASEDIR/../
+source $BASEDIR/../.env
+
+MIGRATION_NAME=$1
+if [ -z "$MIGRATION_NAME" ]; then
+  ./node_modules/.bin/migrate -d $MONGODB_URI --es6 --autosync up --es6 --autosync
+else
+  ./node_modules/.bin/migrate -d $MONGODB_URI up $MIGRATION_NAME --es6 --autosync
+fi
