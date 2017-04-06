5fb89cedddd1e8b100392b2f410d8e3cbc2bf633
Max Fowler <maxhfowler@gmail.com>
2017-04-06 16:11:05 -0400

    firstTimeCaller migration

---
+++ backend/migrations/1491508287665-firstTimeCaller.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const { User, UserConversation } = require('../app/models')
+const logMessage = require('../utilities/logHelper').logMessage
+
+dotenv.load()
+
+mongoose.Promise = require('es6-promise')
+mongoose.connect(process.env.MONGODB_URI)
+
+/**
+ * Make any changes you need to make to the database here
+ */
+export async function up () {
+  // Write migration here
+  await logMessage('++ running migration')
+  const users = await User.find({})
+  for (let user of users) {
+    const userConversationCount = await UserConversation.count({ user: user._id }).exec()
+    const isFirstTimeCaller = userConversationCount <= 1
+    await logMessage(`++ ${user.firstName} ${user.lastName} has ${userConversationCount} conversations: ${isFirstTimeCaller}`)
+    user.firstTimeCaller = isFirstTimeCaller
+    await user.save()
+  }
+  await logMessage('++ completed migration')
+}
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+export async function down () {
+  // Write migration here
+}
