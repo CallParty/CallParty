2b882c414760d406c072512a56c3bbed48ace42e
Max Fowler <maxhfowler@gmail.com>
2017-04-20 16:47:00 -0400

    Add other pages

--- backend/app/utilities/multiTenant.js
+++ backend/app/utilities/multiTenant.js
@@ -3,7 +3,9 @@ const secrets = require('../../devops/secret_files/secret.json')
 function getPageFromId(recipientId) {
   /* returns the string name of the facebook page, which is associated with the given fbId */
   const idToPage = {
-    2060548600838593: 'callparty5'
+    2060548600838593: 'callparty5',
+    356799708039438: 'callingteststaging',
+    243195752776526: 'callparty',
   }
   return idToPage[recipientId]
 }
