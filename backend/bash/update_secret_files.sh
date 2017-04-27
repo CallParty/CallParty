a5097f112ddfb5d00ffbcebf6c4496e22f990f50
Oren Shoham <oren.avraham.shoham@gmail.com>
2017-04-27 16:20:02 -0400

    Configure ./backend/bash/update_secret_files.sh to automatically update .travis.yml

--- backend/bash/update_secret_files.sh
+++ backend/bash/update_secret_files.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+
 set -e
 BASEDIR=$( cd $(dirname $(dirname $0)) ; pwd -P )
-cd $BASEDIR/devops
-tar cvf secret_files.tar secret_files/*
-travis encrypt-file secret_files.tar
-rm secret_files.tar
+cd $BASEDIR/../
+tar cvf backend/devops/secret_files.tar backend/devops/secret_files/*
+travis encrypt-file backend/devops/secret_files.tar --add before_deploy
+rm backend/devops/secret_files.tar
