9d575442c7ed21ea047b452b477eea6c4a89690f
Max Fowler <maxhfowler@gmail.com>
2017-05-02 18:02:25 -0400

    Borrowed from on campaign action detail

--- frontend/app/CampaignActionDetail.js
+++ frontend/app/CampaignActionDetail.js
@@ -209,7 +209,7 @@ export default class CampaignActionDetail extends React.Component {
     if (this.state.action.targetingType === 'segmenting') {
       targeting = [memberTypeTargeting, ...partyTargeting, ...committeeTargeting, ...districtTargeting]
     } else if (this.state.action.targetingType === 'borrowed') {
-      targeting = [`action: ${this.state.action.targetAction.title} `]
+      targeting = [`borrowed from: ${this.state.action.targetAction.label} `]
     }
     return (
       <ul className="targeting">
